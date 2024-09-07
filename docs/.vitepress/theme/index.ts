f936c2edde28b3cf3c9e3965907e1d277759a574
LeeGuanLi <lee@mtftm.com>
2024-09-07 11:03:22 +0800

    Update Vitepress theme to extend default theme and remove unused CSS file

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,7 @@
 // .vitepress/theme/index.ts
+import DefaultTheme from 'vitepress/theme' // 导入 VitePress 的默认主题
 import './style.css' // 引入自定义的 style.css 文件
-import DefaultTheme from './style.css'
-
 
 export default {
-  ...DefaultTheme, // 继承默认主题
+  ...DefaultTheme, // 继承 VitePress 默认主题
 }
