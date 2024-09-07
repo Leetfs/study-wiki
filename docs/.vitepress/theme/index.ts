82227db5a8df862e3b88b93db64ebe07bdf44f33
LeeGuanLi <lee@mtftm.com>
2024-09-07 10:51:10 +0800

    Update Vitepress theme to extend default theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,3 +1,12 @@
 // https://vitepress.dev/guide/custom-theme
 import './style.css'
 
+import { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme' // 导入默认主题
+
+const theme: Theme = {
+  ...DefaultTheme,
+  // 你可以在这里扩展默认主题
+}
+
+export default theme
