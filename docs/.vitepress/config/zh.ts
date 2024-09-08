02db1ef64b02be63d1bfb1f4f86985abca352d32
LeeGuanLi <lee@mtftm.com>
2024-09-08 10:50:24 +0800

    Refactor sidebar generation function to use correct relative path for directory

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -3,12 +3,10 @@ import { defineConfig, type DefaultTheme } from 'vitepress'
 
 import { getSidebar } from '../../../src/node/utils/sidebar'; // 根据项目结构调整路径
 
-export default {
-  themeConfig: {
-    sidebar: {
-      '/computer/': getSidebar('computer'), // 动态生成的侧边栏，用于 /reference 路径
-      '/web/': getSidebar('web'), // 你可以根据需要使用动态生成的
-    }
+export const zh = {
+  sidebar: {
+    '/web/': getSidebar('web'),  // 传入相对路径
+    '/computer/': getSidebar('computer')
   }
 };
 
