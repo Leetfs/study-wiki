2d3d232acb3c875a961d6c15c5d1cafad1e6b376
Lee <lee@mtftm.com>
2024-09-16 22:49:52 +0800

    Update 部署到 Cloudflare Pages 触发分支 to dev

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vitepress'
 import { shared } from './shared'
 import { zh } from './zh'
+import readingTimePlugin from 'vitepress-reading-time'
 
 export default defineConfig({
+  plugins: [readingTimePlugin()],
   ...shared,
   locales: {
-    root: { label: '简体中文', ...zh },
+    root: { label: '简体中文', ...zh }
   }
 })
